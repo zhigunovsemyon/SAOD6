53628fdfc5f09931e5c0408b4c5d1dae5a870ed2
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-11-11 21:24:16 +0300

    Сделал функции вставки элемента

--- src/main.c
+++ src/main.c
@@ -2,9 +2,9 @@
 Напишите алгоритм и программу, меняющую местами m-й и
 n-й элементы списка.
 */
-#include <stdio.h> /*printf()*/
+#include <stdio.h>  /*printf()*/
 #include <stdlib.h> /*EXIT_SUCCESS; malloc(); free()*/
-#include <time.h> /*time()*/
+#include <time.h>   /*time()*/
 #define DATATYPE double
 
 /*Элемент списка*/
@@ -28,6 +28,25 @@ ListMember *ListMemberAllocate(DATATYPE const data) {
 	return el;
 }
 
+/*Вставка элемента в начало существующего списка.
+ * Возврат указателя на вставленный элемент*/
+ListMember *InsertMemberInBegin(ListMember **ptr, DATATYPE const data) {
+	ListMember *memb = ListMemberAllocate(data);
+	if (!memb /*==NULL*/)
+		return NULL;
+	memb->next = (*ptr);
+	*ptr = memb;
+	return memb;
+}
+
+/*Вставка элемента через n других
+ Возврат указателя на вставленный элемент*/
+ListMember *InsertMemberAfterN(ListMember **ptr, int const n,
+			       DATATYPE const data) {
+	return (n /*!=0*/) ? InsertMemberAfterN(ptr, n - 1, data)
+			   : InsertMemberInBegin(ptr, data);
+}
+
 /*Удаление крайнего элемента*/
 void Remove1stElement(ListMember **ptr) {
 	ListMember *member = *ptr;
@@ -44,18 +63,18 @@ void RemoveNthElement(ListMember **ptr, int const n) {
 }
 
 /*Удаление целого листа*/
-void RemoveWholeList (ListMember ** ptr){
-	while(*ptr)
+void RemoveWholeList(ListMember **ptr) {
+	while (*ptr)
 		Remove1stElement(ptr);
 }
 
-void SwapNum(int *const a, int *const b){
+void SwapNum(int *const a, int *const b) {
 	int tmp = *a;
 	*a = *b;
 	*b = tmp;
 }
 
-int GetRandNum(int a, int b){
+int GetRandNum(int a, int b) {
 	if (a > b)
 		SwapNum(&a, &b);
 
@@ -66,22 +85,22 @@ int main(void) {
 	srand((unsigned int)time(NULL));
 
 	ListMember *head, /*Указатель на первый элемент*/
-		   *tail; /*Указатель на крайний элемент*/
+		*tail; /*Указатель на крайний элемент*/
 	head = ListMemberAllocate((DATATYPE)GetRandNum(-10, 10));
-	if (!head/*==NULL*/)
+	if (!head /*==NULL*/)
 		return EXIT_FAILURE;
 	tail = head;
 
-	for(int i = 1; i < 100; ++i){
-		tail->next = ListMemberAllocate((DATATYPE)GetRandNum(-10, 10));
+	for (int i = 1; i < 100; ++i) {
+		tail->next = ListMemberAllocate((DATATYPE)i);
+		// tail->next = ListMemberAllocate((DATATYPE)GetRandNum(-10, 10));
 		tail = tail->next;
 	}
 
-	while(head/*!=NULL*/){
-		printf("%lf\n", head->data);
+	while (head /*!=NULL*/) {
+		printf("%.2lf\n", head->data);
 		Remove1stElement(&head);
 	}
-
 
 	return EXIT_SUCCESS;
 }
