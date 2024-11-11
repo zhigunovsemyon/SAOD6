dd31099d1a1a395dc5600d841d106b3ee002e10a
zhigunovsemyon <zhigunovsemen@gmail.com>
2024-11-11 19:41:26 +0300

    Сделал функции удаления n-ного и крайнего эл-та и всего листа

--- src/main.c
+++ src/main.c
@@ -2,9 +2,10 @@
 Напишите алгоритм и программу, меняющую местами m-й и
 n-й элементы списка.
 */
+#include <stdio.h> /*printf()*/
 #include <stdlib.h> /*EXIT_SUCCESS; malloc(); free()*/
+#include <time.h> /*time()*/
 #define DATATYPE double
-
 
 /*Элемент списка*/
 typedef struct ListMember ListMember;
@@ -13,8 +14,9 @@ struct ListMember {
 	ListMember *next;
 };
 
-ListMember *ListMemberAllocate(DATATYPE const data){
-	ListMember * el; /*Создаваемый элемент*/
+/*Создание элемента*/
+ListMember *ListMemberAllocate(DATATYPE const data) {
+	ListMember *el; /*Создаваемый элемент*/
 	/*Выделение памяти под элемент, проверка*/
 	el = (ListMember *)malloc(sizeof(ListMember));
 	if (!el)
@@ -26,8 +28,60 @@ ListMember *ListMemberAllocate(DATATYPE const data){
 	return el;
 }
 
+/*Удаление крайнего элемента*/
+void Remove1stElement(ListMember **ptr) {
+	ListMember *member = *ptr;
+	if (!member /*==NULL*/)
+		return;
+	*ptr = member->next;
+	free(member);
+}
 
-int main (void) {
+/*Удаление n-ного элемента*/
+void RemoveNthElement(ListMember **ptr, int const n) {
+	return (n /*!=0*/) ? RemoveNthElement(ptr, n - 1)
+			   : Remove1stElement(ptr);
+}
+
+/*Удаление целого листа*/
+void RemoveWholeList (ListMember ** ptr){
+	while(*ptr)
+		Remove1stElement(ptr);
+}
+
+void SwapNum(int *const a, int *const b){
+	int tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+int GetRandNum(int a, int b){
+	if (a > b)
+		SwapNum(&a, &b);
+
+	return a + rand() % (++b - a);
+}
+
+int main(void) {
+	srand((unsigned int)time(NULL));
+
+	ListMember *head, /*Указатель на первый элемент*/
+		   *tail; /*Указатель на крайний элемент*/
+	head = ListMemberAllocate((DATATYPE)GetRandNum(-10, 10));
+	if (!head/*==NULL*/)
+		return EXIT_FAILURE;
+	tail = head;
+
+	for(int i = 1; i < 100; ++i){
+		tail->next = ListMemberAllocate((DATATYPE)GetRandNum(-10, 10));
+		tail = tail->next;
+	}
+
+	while(head/*!=NULL*/){
+		printf("%lf\n", head->data);
+		Remove1stElement(&head);
+	}
+
 
 	return EXIT_SUCCESS;
 }
